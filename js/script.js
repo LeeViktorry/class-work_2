b0347265d39a281497820f2c17a50a1e2b350669
LeeViktorry <viktoriahaliullina160@gmail.com>
2021-06-30 17:48:27 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// function Names(){
+//     var count = +prompt('Count')
+//     var arr = []
+//     arr.length = count
+//     for(var i = 0; i < arr.length; i++){
+//         arr[i] = prompt('Name')
+//     }
+//     console.log(arr);
+    
+//     var v = prompt('Name')
+//     let b = []
+//     let j = 0
+//     for(var l = 0; l < arr.length; l++){
+//         if(arr[l] === v){
+//             b[j] = arr[l]
+//             j++
+//         }
+//     }
+
+//     let changedArray = b.join('').split('').reverse().join('')
+//     console.log(changedArray);
+// }
+// Names()
+
+
+// const Name = count =>{
+//     let arr = []
+//     for(var i = 0; i < count; i++){arr.push(prompt('Name'))}console.log(arr);
+//     let v = prompt('name')
+//     let b = []
+//     for(var l = 0; l < arr.length; l++)
+//     {v===arr[l] ? console.log(arr[l]) : alert('Hello world')}
+// }
+// Name(+prompt('Count'))
+
+
+// #3
+
+function Calc(ask){
+    
+    num == -Math.abs(asknum)
+    console.log(num);
+}
+Calc(+prompt('Enter the number'))
