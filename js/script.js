83e12e38f659e93581894927cb9bf8f78b3815fd
LeeViktorry <viktoriahaliullina160@gmail.com>
2021-07-02 16:49:08 +0600

    first

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// #1/1
+
 // function Names(){
 //     var count = +prompt('Count')
 //     var arr = []
@@ -22,6 +24,8 @@
 // }
 // Names()
 
+ 
+// #1/2
 
 // const Name = count =>{
 //     let arr = []
@@ -34,11 +38,78 @@
 // Name(+prompt('Count'))
 
 
-// #3
 
-function Calc(ask){
-    
-    num == -Math.abs(asknum)
-    console.log(num);
-}
-Calc(+prompt('Enter the number'))
+
+// #2/1
+
+// var counts = +prompt('narr[j].length > ? = narr[j] = 0')
+
+// function calc(count){
+//     var arr = [1, 2, 3, 4, 5, 'hio', 'boi', 'jio', 'sdfghjkl', 'fikavvika', 'dianch']
+//     var narr = []
+//     for(var i = 0; i < arr.length; i++){
+//         arr.push(prompt(i+1))
+//         if(typeof arr[i] === 'string'){
+//             narr.push(arr[i])
+//             for(var j =0; j < narr.length; j++){
+//                 if (narr[j].length > count){
+//                     narr[j] = 0
+//                 }
+//             }
+//         }
+//     }
+//     console.log(arr);
+//     console.log(narr);
+// }
+// calc(counts)
+
+// #2/2
+
+// var counts = +prompt('narr[j].length > ? = narr[j] = 0')
+
+// const calc = count => {
+//     var arr = [1, 2, 3, 4, 5, 'hio', 'boi', 'jio', 'sdfghjkl', 'fikavvika', 'dianch']
+//     var narr = []
+//     for(var i = 0; i < arr.length; i++){
+//         arr.push(prompt(i+1))
+//         if(typeof arr[i] === 'string'){
+//             narr.push(arr[i])
+//             for(var j =0; j < narr.length; j++){
+//                 if (narr[j].length > count){
+//                     narr[j] = 0
+//                 }
+//             }
+//         }
+//     }
+//     console.log(arr);
+//     console.log(narr);
+// }
+
+
+// #3/1
+
+// function Calc(){
+//     var count = +prompt('Count')
+//     var arr = []
+//     arr.length = count
+//     for(var i = 0; i < arr.length; i++){
+//         arr[i] = -Math.abs(Math.floor(Math.random() * 21))
+//     }
+//     console.log(arr);
+// }
+// Calc()
+
+
+// #3/2
+
+// var counts = +prompt('Count')
+
+// const call = count => {
+//     var arr = []
+//     arr.length = count
+//     for(var i = 0; i < arr.length; i++){
+//         arr.push(-Math.abs(Math.floor(Math.random() * 21)))
+//     }
+//     console.log(arr);
+// }
+// call(counts)
